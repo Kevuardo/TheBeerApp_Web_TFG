920862cfdd3150b90d4f21a44e7a0e4baf2c94e6
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2018-04-17 21:17:24 +0200

    Almacena como constantes las expresiones regulares
    Expresiones regulares de validación

---
+++ js/regex.js
@@ -0,0 +1,10 @@
+/* Patrones del index.html */
+const PATRON_NICK = new RegExp(/^[a-zA-Z0-9_]{5,15}$/); /* Patrón del nick de usuario. */
+const PATRON_EMAIL = new RegExp
+  (/^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/); /* Patrón del e-mail de usuario. */
+const PATRON_PWD = new RegExp
+  (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/); /* Patrón del password de usuario. */
+
+/* Otras constantes. */
+const EDAD_MINIMA = 18; /* Edad mínima de uso. */
+const EDAD_MAXIMA = 130; /* Edad máxima de uso (temporal). */
